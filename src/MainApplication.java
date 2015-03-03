728ff5250e31f23ef9de9f41e8719ad27e4575ee
Pavel Kastrykin <offerent@gmail.com>
2015-03-03 23:29:14 +0200

    added logger

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -2,16 +2,31 @@ import com.pavel.textparser.task.TaskNumberOne;
 import com.pavel.textparser.entity.DividedText;
 import com.pavel.textparser.logics.TextManager;
 import com.pavel.textparser.task.TaskNumberTwo;
+import org.apache.log4j.Logger;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;import java.lang.String;
 
 
 public class MainApplication {
 
-    public static void main(String[] args) throws IOException{
+    public static Logger logger = Logger.getLogger(MainApplication.class);
+
+    public static void main(String[] args){
+
 
         TextManager textManager = new TextManager();
-        String text = textManager.readFile("d:/1.txt");
+        String text = "";
+        try {
+            text = textManager.readFile("d:/1.txt");
+        }
+        catch (FileNotFoundException e){
+            logger.error("File was not found!", e);
+        }
+        catch (IOException e){
+            logger.error("Problem reading file!", e);
+        }
+
         DividedText dividedText = new DividedText(text);
 
         TaskNumberOne taskNumberOne = new TaskNumberOne(dividedText);
